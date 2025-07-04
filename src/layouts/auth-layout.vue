6996c7a670e17962a0b05619058c92cabcbcd367
itsslava <77506882+itsslava@users.noreply.github.com>
2025-07-04 20:06:42 +0300

    feat(auth): implement auth routes & layout

---
+++ src/layouts/auth-layout.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import LogoIcon from '@/icons/logo-icon.vue';
+</script>
+<template>
+  <div>
+    <main class="auth-layout">
+      <LogoIcon width="202" height="213" />
+      <RouterView />
+    </main>
+  </div>
+</template>
+
+<style scoped>
+.auth-layout {
+  background-image: url('../assets/bg-img.png');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  width: 100%;
+  min-height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 40px;
+}
+</style>
--- src/routes.ts
+++ src/routes.ts
@@ -7,6 +7,28 @@ export const router = createRouter({
       path: '/',
       redirect: '/main',
     },
+
+    {
+      path: '/auth',
+      component: () => import('@/layouts/auth-layout.vue'),
+      children: [
+        {
+          path: 'welcome',
+          name: 'welcome',
+          component: () => import('@/views/auth/welcome-view.vue'),
+        },
+        {
+          path: 'login',
+          name: 'login',
+          component: () => import('@/views/auth/login-view.vue'),
+        },
+        {
+          path: 'register',
+          name: 'register',
+          component: () => import('@/views/auth/register-view.vue'),
+        },
+      ],
+    },
     {
       path: '/main',
       component: () => import('@/layouts/main-layout.vue'),
