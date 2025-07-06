d8d4d7c12a4e5110122e384b75f4d2d81c88e942
itsslava <77506882+itsslava@users.noreply.github.com>
2025-07-06 15:14:37 +0300

    chore(eslint & api): update ESLint config and extend API routes

--- src/api.ts
+++ src/api.ts
@@ -2,6 +2,11 @@ import axios from 'axios';
 
 export const API_ROUTES = {
   meditations: 'meditations',
+  profile: 'profile',
+  auth: {
+    login: '/auth/login',
+    register: '/auth/register',
+  },
 };
 
 export const http = axios.create({ baseURL: 'http://localhost:3000/api/', timeout: 10000 });
